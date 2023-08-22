f3c4fcbba01e81d151ef07cd9098bea4798df7dd
Kangchuho <reddokk@gmail.com>
2023-08-22 22:33:30 +0900

    수정1

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
+import Nav from './components/Nav';
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer ,
+   CustomerReviews , Subscribe, Footer} from './sections';
 
 const App = () => {
   return (
-    <main>
-      Nav
+    <main className='relative'>
+      <Nav />
       <section className='xl:padding-1 wide:padding-r padding-b' >
-        Hero
+        <Hero />
       </section>
       <section className='padding'>
-        PopularProducts
+        <PopularProducts />
       </section>
       <section className='padding'>
-        SuperQuality
+        <SuperQuality />
       </section>
       <section className='padding-x pay-10'>
-        Services
+        <Services />
       </section>
       <section className='padding'>
-        SpecialOffer
+        <SpecialOffer />
       </section>
       <section className='bg-pale-blue padding'>
-        CustomerReviews
+        <CustomerReviews />
       </section>
       <section className='padding-x sm:py-32 py-16 w-full'>
-        Subscribe
+        <Subscribe />
       </section>
       <section className='bg-black padding-x padding-t pb-8'>
-        Footer
+        <Footer />
       </section>
     </main>
   )
